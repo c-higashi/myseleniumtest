02a88244e9bf5377e9ea8b691126cb5f6eae3326
Chieko Higashi <chigashi@Chiekos-MacBook-Pro.local>
2025-02-17 13:52:35 +0900

    Extracted driver initiation.

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # myseleniumtest
 
-To run the test please run the following from the project root directory:
+To run the test please run the following:
 ```bash
+git clone https://github.com/c-higashi/myseleniumtest.git
+cd myseleniumtest
 ./gradlew --rerun-tasks test
 ```
--- src/test/java/MySeleniumTest.java
+++ src/test/java/MySeleniumTest.java
@@ -5,70 +5,75 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
 
 public class MySeleniumTest {
 
+    private WebDriver driver;
+
+    @BeforeClass
+    void beforeClass() {
+        driver = new ChromeDriver();
+    }
+
+    @AfterClass
+    void afterClass() {
+        driver.close();
+    }
+
     @Test
     void testDoButtons()
     {
-        final WebDriver driver = new ChromeDriver();
-        try {
-            driver.get("https://yltrue.com/automation/");
+        driver.get("https://yltrue.com/automation/");
 
-            // log in
-            login(driver, "admin", "admin");
+        // log in
+        login("admin", "admin");
 
-            WebElement doOneButton = (new WebDriverWait(driver,Duration.ofSeconds(5)))
-                    .until(ExpectedConditions.visibilityOfElementLocated(By.id("do1")));
-            doOneButton.click();
+        // Click DO1! button.
+        WebElement doOneButton = (new WebDriverWait(driver,Duration.ofSeconds(5)))
+                .until(ExpectedConditions.visibilityOfElementLocated(By.id("do1")));
+        doOneButton.click();
 
-            // TODO - Find a better way to get attribute "disabled"
-            String doOneButtonDisabled = doOneButton.getAttribute("disabled");
-            Assert.assertNotNull(doOneButtonDisabled);
+        // TODO - Find a better way to get attribute "disabled"
+        String doOneButtonDisabled = doOneButton.getAttribute("disabled");
+        Assert.assertNotNull(doOneButtonDisabled);
 
-            WebElement doTwoButton = driver.findElement(By.id("do2"));
-            doTwoButton.click();
-            String doTwoButtonDisabled = doTwoButton.getAttribute("disabled");
-            Assert.assertNotNull(doTwoButtonDisabled);
-        } finally {
-            driver.close();
-        }
+        // Click DO2! button.
+        WebElement doTwoButton = driver.findElement(By.id("do2"));
+        doTwoButton.click();
+        String doTwoButtonDisabled = doTwoButton.getAttribute("disabled");
+        Assert.assertNotNull(doTwoButtonDisabled);
     }
 
     @Test
     void testChangeFontSize() {
-        final WebDriver driver = new ChromeDriver();
-        try
-        {
-            driver.get("https://yltrue.com/automation/");
-            // log in
-            login(driver, "admin", "admin");
+        driver.get("https://yltrue.com/automation/");
+        // log in
+        login("admin", "admin");
 
-            WebElement textFontSize = driver.findElement(By.id("textFontSize"));
-            String fontSize = textFontSize.getAttribute("style");
-            Assert.assertEquals(fontSize, "font-size: 20px;");
+        // Verify the default font size.
+        WebElement textFontSize = driver.findElement(By.id("textFontSize"));
+        String fontSize = textFontSize.getAttribute("style");
+        Assert.assertEquals(fontSize, "font-size: 20px;");
 
-            // Increase the font size
-            WebElement increaseFontSizeButton = driver.findElement(By.id("btnIncreaseFont"));
-            increaseFontSizeButton.click();
-            fontSize = textFontSize.getAttribute("style");
-            Assert.assertEquals(fontSize, "font-size: 23px;");
+        // Increase the font size.
+        WebElement increaseFontSizeButton = driver.findElement(By.id("btnIncreaseFont"));
+        increaseFontSizeButton.click();
+        fontSize = textFontSize.getAttribute("style");
+        Assert.assertEquals(fontSize, "font-size: 23px;");
 
-            // Decrease the font size
-            WebElement decreaseFontSizeButton = driver.findElement(By.id("btnDecreaseFont"));
-            decreaseFontSizeButton.click();
-            fontSize = textFontSize.getAttribute("style");
-            Assert.assertEquals(fontSize, "font-size: 20px;");
-        }
-        finally {
-            driver.close();
-        }
+        // Decrease the font size.
+        WebElement decreaseFontSizeButton = driver.findElement(By.id("btnDecreaseFont"));
+        decreaseFontSizeButton.click();
+        fontSize = textFontSize.getAttribute("style");
+        Assert.assertEquals(fontSize, "font-size: 20px;");
     }
 
-    private void login(WebDriver driver, String username, String password) {
+    private void login(String username, String password) {
         WebElement usernameField = (new WebDriverWait(driver,Duration.ofSeconds(5)))
                 .until(ExpectedConditions.visibilityOfElementLocated(By.name("username")));
         WebElement passwordField = (new WebDriverWait(driver,Duration.ofSeconds(5)))
