13f1a283e4fcb8253eb57aa685a944a9c415b7f7
Chieko Higashi <chigashi@Chiekos-MacBook-Pro.local>
2025-02-24 16:50:36 +0900

    cleanup: LoginPage

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -1,7 +1,7 @@
 package pages;
 
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -9,18 +9,23 @@ import org.testng.Assert;
 
 import java.time.Duration;
 
+/**
+ * Represents the login page of https://yltrue.com/automation
+ */
 public class LoginPage {
-    private WebDriver driver;
+    private final WebDriver driver;
 
-    public LoginPage(WebDriver driver)
-    {
+    public LoginPage(WebDriver driver) {
         this.driver = driver;
     }
 
+    /**
+     * Performs a login using the provided username and password
+     */
     public void login(String username, String password) {
-        WebElement usernameField = (new WebDriverWait(driver, Duration.ofSeconds(5)))
+        WebElement usernameField = new WebDriverWait(driver, Duration.ofSeconds(5))
                 .until(ExpectedConditions.visibilityOfElementLocated(By.name("username")));
-        WebElement passwordField = (new WebDriverWait(driver,Duration.ofSeconds(5)))
+        WebElement passwordField = new WebDriverWait(driver, Duration.ofSeconds(5))
                 .until(ExpectedConditions.visibilityOfElementLocated(By.name("password")));
 
         usernameField.sendKeys(username);
@@ -29,7 +34,7 @@ public class LoginPage {
         WebElement loginButton = driver.findElement(By.name("login"));
         loginButton.click();
 
-        String welcomeMsg = (new WebDriverWait(driver,Duration.ofSeconds(5)))
+        String welcomeMsg = new WebDriverWait(driver, Duration.ofSeconds(5))
                 .until(ExpectedConditions.visibilityOfElementLocated(By.id("welcomeMsg"))).getText();
         Assert.assertTrue(welcomeMsg.contains("Welcome to the Main page!"), "Log in failed!");
     }
